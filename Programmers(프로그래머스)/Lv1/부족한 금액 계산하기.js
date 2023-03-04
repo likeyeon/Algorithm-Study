7ab08615c73c2a2bab70aa75b58dcf990fc8c6b9
likeyeon <chy98csy00@naver.com>
2023-03-04 14:17:00 +0900

    20230304

---
+++ Programmers(프로그래머스)/Lv1/부족한 금액 계산하기.js
@@ -0,0 +1,10 @@
+function solution(price, money, count) {
+  let total = 0;
+
+  for (let i=1; i<=count; i++)
+    total += price*i;
+
+  return total > money ? total-money : 0;
+}
+
+console.log(solution(3, 35, 4));
