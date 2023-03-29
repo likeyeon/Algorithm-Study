44dec7b6e7785d7a76e1a6a6798a2be9191531d1
likeyeon <chy98csy00@naver.com>
2023-03-30 01:29:08 +0900

    20230330

--- Programmers(프로그래머스)/Lv1/정렬/K번째수.js
+++ Programmers(프로그래머스)/Lv1/정렬/K번째수.js
@@ -1,12 +1,11 @@
 function solution(array, commands) {
   let answer = [];
 
-  for(let i=0; i<commands.length; i++){ // commands의 길이만큼 반복
+  for(let i=0; i<commands.length; i++) // commands의 길이만큼 반복
     answer.push((array.slice(commands[i][0]-1,commands[i][1])).sort((a,b)=>a-b)[commands[i][2]-1]);
     // slice를 이용해 i부터 j만큼 array를 슬라이싱
     // 슬라이싱한 후, sort로 array 오름차순 정렬
     // 정렬된 array에서 k에 해당하는 수를 찾아 answer에 push
-  }
   return answer;
 }
 
