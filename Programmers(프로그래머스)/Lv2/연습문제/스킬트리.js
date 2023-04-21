16a9de5350b0ec693132c2eca5bc30c890c61972
likeyeon <chy98csy00@naver.com>
2023-04-22 02:21:35 +0900

    lv2 스킬트리

--- Programmers(프로그래머스)/Lv2/연습문제/스킬트리.js
+++ Programmers(프로그래머스)/Lv2/연습문제/스킬트리.js
@@ -6,12 +6,13 @@
 
 function solution(skill, skill_trees) {
   let answer = 0;
-  const regex = new RegExp(`[^${skill}]`, "g");
-  skill_trees = skill_trees.map((x) => x.replace(regex, ""));
+  const regex = new RegExp(`[^${skill}]`, "g"); // RegExp 객체 생성
+  skill_trees = skill_trees.map((x) => x.replace(regex, "")); // skill 문자열에 해당하지 않는 문자 제거
 
   for (let i = 0; i < skill_trees.length; i++) {
     for (let j = 0; j < skill_trees[i].length; j++) {
       if (skill_trees[i][j] !== skill[j]) {
+        // skill 순서와 맞지않는 스킬트리 카운트 X
         answer--;
         break;
       }
