e95134f880f7dd205ae893973ff467e248d8f6bd
likeyeon <chy98csy00@naver.com>
2023-08-15 16:41:04 +0900

    [백준] 17413번 단어 뒤집기 2 - 미해결

---
+++ BAEKJOON(백준)/단계별 문제/200) 자료구조 1/단어 뒤집기 2.js
@@ -0,0 +1,17 @@
+// 미해결
+// 17413번 | 단어 뒤집기 2
+// 문제 : https://www.acmicpc.net/problem/17413
+// 참고 : https://gigibean.tistory.com/18
+
+const fs = require("fs");
+
+// 백준
+// const readFileSyncAddress = '/dev/stdin';
+
+// VSC
+const readFileSyncAddress = __dirname + "/input.txt";
+
+// input 값 저장
+const input = fs.readFileSync(readFileSyncAddress).toString().trim();
+
+// 코드
